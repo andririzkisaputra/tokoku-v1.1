1ecc81a06035fd877a5882d9a06433d28cfae365
andririzki <andri.rizki007@gmail.com>
2021-12-23 14:49:39 +0700

    main layout

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -16,24 +16,27 @@ AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
-<html>
-<head>
-    <!-- Basic -->
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <!-- Mobile Metas -->
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <!-- Site Metas -->
-    <meta name="keywords" content="" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <?php $this->registerCsrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
-</head>
-<body>
-<?php $this->beginBody() ?>
+<html lang="<?= Yii::$app->language ?>">
+    <head>
+        <meta charset="<?= Yii::$app->charset ?>">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?php $this->registerCsrfMetaTags() ?>
+        <title><?= Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
+    </head>
+    <body>
+        <?php $this->beginBody() ?>
 
+        <style>
+            .bg-menu{
+                background:#034f84;
+                color:white !important;
+            }
+            .navbar-header > a{
+                color:white;
+            }
+        </style>
 
         <div class="wrap">
             <?php
@@ -41,7 +44,7 @@ AppAsset::register($this);
             // $view = $cart->getCount();
 
             NavBar::begin([
-                'brandLabel' => 'HARBOLNAS',
+                'brandLabel' => 'TokoKu',
                 'brandUrl' => Yii::$app->homeUrl,
                 'options' => ['class' => 'bg-menu mynavbar navbar-fixed-top'],
             ]);
@@ -49,7 +52,14 @@ AppAsset::register($this);
                 'options' => ['class' => 'navbar-nav mynavbar  navbar-right'],
                 'items' => [
                     ['label' => '<span class="glyphicon glyphicon-home"></span> Beranda', 'url' => ['/osengmercon/default/index', 'slug' => 'oseng-mercon-bu-narti']],
-                    ['label' => '<span class="glyphicon glyphicon-shopping-cart"></span> Keranjang <span class="label label-success">$view"</span>"', 'url' => ['/osengmercon/default/cart']],
+//                    ['label' => '<span class="glyphicon glyphicon-search"></span> Produk',
+//                        'items' => [
+//                            ['label' => 'Face Products', 'url' => ['/site/face']],
+//                            ['label' => 'Hair Products', 'url' => ['/site/hair']],
+//                            ['label' => 'Body Products', 'url' => ['/site/body']],
+//                        ]
+//                    ],
+                    ['label' => '<span class="glyphicon glyphicon-shopping-cart"></span> Keranjang <span class="label label-success">view"</span>"', 'url' => ['/osengmercon/default/cart']],
                 ],
                 'encodeLabels' => false,
             ]);
@@ -70,90 +80,15 @@ AppAsset::register($this);
             </div>
         </div>
 
-<main role="main" class="flex-shrink-0">
-    <!-- <div class="container"> -->
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
-    <!-- </div> -->
-</main>
+        <footer class="footer" style="background:#034f84;">
+            <div class="container">
+                <p class="pull-left" style="color:white">&copy; TokoKu <?= date('Y') ?></p>
 
-<!-- footer start -->
-<footer>
-   <div class="container">
-      <div class="row">
-         <div class="col-md-4">
-             <div class="full">
-                <div class="logo_footer">
-                  <a href="#"><img width="210" src="<?=  Url::base(true).'/images/logo.png' ?>" alt="#" /></a>
-                </div>
-                <div class="information_f">
-                  <p><strong>ADDRESS:</strong> 28 White tower, Street Name New York City, USA</p>
-                  <p><strong>TELEPHONE:</strong> +91 987 654 3210</p>
-                  <p><strong>EMAIL:</strong> yourmain@gmail.com</p>
-                </div>
-             </div>
-         </div>
-         <div class="col-md-8">
-            <div class="row">
-            <div class="col-md-7">
-               <div class="row">
-                  <div class="col-md-6">
-               <div class="widget_menu">
-                  <h3>Menu</h3>
-                  <ul>
-                     <li><a href="#">Home</a></li>
-                     <li><a href="#">About</a></li>
-                     <li><a href="#">Services</a></li>
-                     <li><a href="#">Testimonial</a></li>
-                     <li><a href="#">Blog</a></li>
-                     <li><a href="#">Contact</a></li>
-                  </ul>
-               </div>
+                <p class="pull-right" style="color:white">Powered by FasaPay Indonesia</p>
             </div>
-            <div class="col-md-6">
-               <div class="widget_menu">
-                  <h3>Account</h3>
-                  <ul>
-                     <li><a href="#">Account</a></li>
-                     <li><a href="#">Checkout</a></li>
-                     <li><a href="#">Login</a></li>
-                     <li><a href="#">Register</a></li>
-                     <li><a href="#">Shopping</a></li>
-                     <li><a href="#">Widget</a></li>
-                  </ul>
-               </div>
-            </div>
-               </div>
-            </div>
-            <div class="col-md-5">
-               <div class="widget_menu">
-                  <h3>Newsletter</h3>
-                  <div class="information_f">
-                    <p>Subscribe by our newsletter and get update protidin.</p>
-                  </div>
-                  <div class="form_sub">
-                     <form>
-                        <fieldset>
-                           <div class="field">
-                              <input type="email" placeholder="Enter Your Mail" name="email" />
-                              <input type="submit" value="Subscribe" />
-                           </div>
-                        </fieldset>
-                     </form>
-                  </div>
-               </div>
-            </div>
-            </div>
-         </div>
-      </div>
-   </div>
-</footer>
-<!-- footer end -->
+        </footer>
 
-<?php $this->endBody() ?>
-</body>
+        <?php $this->endBody() ?>
+    </body>
 </html>
 <?php $this->endPage();
