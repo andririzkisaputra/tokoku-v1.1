6889720ad1efa2f6fb4dfb5b7cb5b672118f7df1
andririzki <andri.rizki007@gmail.com>
2022-01-05 16:59:08 +0700

    fix cr code

--- frontend/views/site/pesanan/detailQrCode.php
+++ frontend/views/site/pesanan/detailQrCode.php
@@ -23,10 +23,25 @@ $this->registerJs("
                 'kode_transaksi' : kode_transaksi
             },
             success: function(res){
-                console.log(res);
-                // array = array.join('');
-                // html  = array.toString();
-                // $('#list-qr-code').html(html);
+                let html     = '';
+                let gambar_f = '';
+                let array    = [];
+                console.log(res.data);
+                res.data.map((item, index) => {
+                    gambar_f = '/e-commerce/uploads/frontend/qr_code/'+item.qr_code;
+                    array.push(
+                        '<b>QR Code</b>'
+                        +'<div style=".'"margin: 10px"'.">'
+                            +'<img src='+gambar_f+'>'
+                        +'</div>'
+                        +'<b>Kode Transaksi</b><p>'+item.kode_transaksi+'</p>'
+                        +'<b>Produk</b><p>Rp '+item.harga_produk+'</p>'
+                        +'<b>Ongkir</b><p>Rp '+item.ongkir+'</p>'
+                    );
+                });
+                array = array.join('');
+                html  = array.toString();
+                $('#list-qr-code').html(html);
                 return true;
             },
             error: function(e){
@@ -38,9 +53,8 @@ $this->registerJs("
 ");
 ?>
 
-<div class="site-index">
-    <p><?= $kode_transaksi ?></p>
-  <!-- <div id="list-qr-code"></div> -->
+<div class="site-index" style="text-align: center">
+  <div id="list-qr-code"></div>
   <!-- <div class=""style="text-align: end; margin: 5px 0px 0px 0px">
     <button type="button" name="button" class="btn btn-info pembayaran" data-dismiss="modal" style="">Simpan</button>
   </div> -->
